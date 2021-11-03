6e005738948cf9c8ab04b8277cca4f2f35f55f52
Amir <amirni98@gmail.com>
2021-11-04 00:25:20 +0330

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello');
+const count = 10;
+const apiKey = '0_yIbWZQdTBTKwyEuguc8wEXt8FTk3ye95VLu-MHEVw';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data      = await response.json();
+        console.log(data);
+
+    } catch (error) {
+        //d
+    }
+}
+
+getPhotos();
